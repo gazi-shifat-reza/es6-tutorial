eea0db16e9f4aad5754592ab92a8d8c3e205e53d
shifatreza <shifatreza44@gmail.com>
2021-02-01 15:07:37 +0530

    class javascript

---
+++ es6-tutorial/class.js
@@ -0,0 +1,13 @@
+class Student{          //class declaration
+    constructor(sName, sAge){       //constructor set
+        this.name = sName;
+        this.age = sAge;
+        this.school = 'Rifles public School';
+    }
+}
+
+const student1 = new Student('Rony', 22);   // access the property
+const student2 = new Student('Rishad', 21);
+console.log(student1.school);
+console.log(student2.name);
+console.log(student1, student2);
